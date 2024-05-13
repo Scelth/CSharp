6eeecf94021d9b6e9644098f750b96a55c48b014
Scelth <magomed.guluyev@gmail.com>
2024-05-13 16:06:17 +0400

    Shapes added

---
+++ Shapes/Shapes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public abstract class Shape
+{
+    public abstract double CalculateArea();
+}
+
+public class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public override double CalculateArea()
+    {
+        return Math.PI * Math.Pow(Radius, 2);
+    }
+}
+
+public class Triangle : Shape
+{
+    public double SideA { get; set; }
+    public double SideB { get; set; }
+    public double SideC { get; set; }
+
+    public Triangle(double sideA, double sideB, double sideC)
+    {
+        SideA = sideA;
+        SideB = sideB;
+        SideC = sideC;
+    }
+
+    public override double CalculateArea()
+    {
+        double s = (SideA + SideB + SideC) / 2.0;
+        return Math.Sqrt(s * (s - SideA) * (s - SideB) * (s - SideC));
+    }
+
+    public bool IsRightAngled()
+    {
+        double[] sides = new double[] { SideA, SideB, SideC };
+        Array.Sort(sides);
+        return Math.Abs(Math.Pow(sides[2], 2) - (Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2))) < 0.000001;
+    }
+}
